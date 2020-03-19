bd113396467a0b3c564d18d5f52dab552daab84b
Bruno Leite <brunocleite@gmail.com>
2020-03-19 20:33:05 -0300

    Removes not default module-info from module migration

--- co.brunoleite.modules.migration/co.brunoleite.modules.migration.flower.app/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module co.brunoleite.modules.migration.flower.app {
-    requires co.brunoleite.modules.migration.flower;
-    requires java.logging;
-
-    exports co.brunoleite.modules.migration.flower.app;
-
-}
